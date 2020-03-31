a1979dfc38500de0cbda84b1af00aaf651874a26
Aishwarya Shrikant Mundada <amundada@mtu.edu>
2020-03-31 19:37:44 -0400

    Ukf File edited for Project

--- ukf.h
+++ ukf.h
@@ -21,7 +21,6 @@ class UKF {
    * @param meas_package The latest measurement data of either radar or laser
    */
   void ProcessMeasurement(MeasurementPackage meas_package);
-
   /**
    * Prediction Predicts sigma points, the state, and the state covariance
    * matrix
